e525cea8a531ee2d46b084d64f8bd133491e3eae
AB <a.bierler@xdev-software.de>
2024-06-19 11:29:31 +0200

    Dedup code

--- vaadin-maps-leaflet-flow/src/main/java/software/xdev/vaadin/maps/leaflet/map/AbstractLMapZoomPanOptions.java
+++ vaadin-maps-leaflet-flow/src/main/java/software/xdev/vaadin/maps/leaflet/map/AbstractLMapZoomPanOptions.java
@@ -15,70 +15,13 @@
  */
 package software.xdev.vaadin.maps.leaflet.map;
 
-/**
- * @see <a href="https://leafletjs.com/reference.html#zoom/pan-options">Leaflet docs</a>
- */
 public class AbstractLMapZoomPanOptions<T extends AbstractLMapZoomPanOptions<T>>
-	implements
-	LMapZoomBaseOptions<T>,
-	LMapPanBaseOptions<T>
+	extends AbstractLMapPanOptions<T>
+	implements LMapZoomBaseOptions<T>
 {
-	private Boolean animate;
-	private Double duration;
-	private Double easeLinearity;
-	private Boolean noMoveStart;
-	
-	@Override
-	public Boolean getAnimate()
-	{
-		return this.animate;
-	}
-	
-	@Override
-	public void setAnimate(final Boolean animate)
-	{
-		this.animate = animate;
-	}
-	
 	@Override
 	public T withAnimate(final Boolean animate)
 	{
 		return LMapZoomBaseOptions.super.withAnimate(animate);
-	}
-	
-	@Override
-	public Double getDuration()
-	{
-		return this.duration;
-	}
-	
-	@Override
-	public void setDuration(final Double duration)
-	{
-		this.duration = duration;
-	}
-	
-	@Override
-	public Double getEaseLinearity()
-	{
-		return this.easeLinearity;
-	}
-	
-	@Override
-	public void setEaseLinearity(final Double easeLinearity)
-	{
-		this.easeLinearity = easeLinearity;
-	}
-	
-	@Override
-	public Boolean getNoMoveStart()
-	{
-		return this.noMoveStart;
-	}
-	
-	@Override
-	public void setNoMoveStart(final Boolean noMoveStart)
-	{
-		this.noMoveStart = noMoveStart;
 	}
 }
--- vaadin-maps-leaflet-flow/src/main/java/software/xdev/vaadin/maps/leaflet/map/LMapPanOptions.java
+++ vaadin-maps-leaflet-flow/src/main/java/software/xdev/vaadin/maps/leaflet/map/LMapPanOptions.java
@@ -18,58 +18,6 @@ package software.xdev.vaadin.maps.leaflet.map;
 /**
  * @see <a href="https://leafletjs.com/reference.html#pan-options">Leaflet docs</a>
  */
-public class LMapPanOptions implements LMapPanBaseOptions<LMapPanOptions>
+public class LMapPanOptions extends AbstractLMapPanOptions<LMapPanOptions>
 {
-	private Boolean animate;
-	private Double duration;
-	private Double easeLinearity;
-	private Boolean noMoveStart;
-	
-	@Override
-	public Boolean getAnimate()
-	{
-		return this.animate;
-	}
-	
-	@Override
-	public void setAnimate(final Boolean animate)
-	{
-		this.animate = animate;
-	}
-	
-	@Override
-	public Double getDuration()
-	{
-		return this.duration;
-	}
-	
-	@Override
-	public void setDuration(final Double duration)
-	{
-		this.duration = duration;
-	}
-	
-	@Override
-	public Double getEaseLinearity()
-	{
-		return this.easeLinearity;
-	}
-	
-	@Override
-	public void setEaseLinearity(final Double easeLinearity)
-	{
-		this.easeLinearity = easeLinearity;
-	}
-	
-	@Override
-	public Boolean getNoMoveStart()
-	{
-		return this.noMoveStart;
-	}
-	
-	@Override
-	public void setNoMoveStart(final Boolean noMoveStart)
-	{
-		this.noMoveStart = noMoveStart;
-	}
 }
